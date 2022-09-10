3da65568fbfe8ac98363bc0c76cdf35408a599ce
joescaos <jxexcxo@gmail.com>
2022-09-10 16:02:42 -0500

    details endpoint and view added

--- src/main/java/com/example/employmentApp/controller/MainController.java
+++ src/main/java/com/example/employmentApp/controller/MainController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -20,5 +21,12 @@ public class MainController {
         List<Employment> employmentList = employmentService.findAll();
         model.addAttribute("employments", employmentList);
         return "home";
+    }
+
+    @GetMapping("/details/{id}")
+    public String positionDetails(@PathVariable int id, Model model) {
+        Employment employment = employmentService.getById(id);
+        model.addAttribute("employment", employment);
+        return "details";
     }
 }
