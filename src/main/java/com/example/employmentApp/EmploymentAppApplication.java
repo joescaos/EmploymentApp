18950a299907f89acf741d88fdf44bb71a77db54
joescaos <jxexcxo@gmail.com>
2022-10-24 22:35:52 -0500

    removed unused imports

--- src/main/java/com/example/employmentApp/EmploymentAppApplication.java
+++ src/main/java/com/example/employmentApp/EmploymentAppApplication.java
@@ -1,19 +1,7 @@
 package com.example.employmentApp;
 
-import com.example.employmentApp.model.Category;
-import com.example.employmentApp.repositories.CategoriesRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
 
 @SpringBootApplication
 public class EmploymentAppApplication {
