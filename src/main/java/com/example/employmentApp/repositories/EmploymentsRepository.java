8c55347833c0007359d9d1d59ed2b7b01cd5ceea
joescaos <jxexcxo@gmail.com>
2022-10-26 21:52:34 -0500

    custom queries added in employments repository

--- src/main/java/com/example/employmentApp/repositories/EmploymentsRepository.java
+++ src/main/java/com/example/employmentApp/repositories/EmploymentsRepository.java
@@ -3,6 +3,16 @@ package com.example.employmentApp.repositories;
 import com.example.employmentApp.model.Employment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EmploymentsRepository extends JpaRepository<Employment, Integer> {
+
+    List<Employment> findByStatus(String status);
+
+    List<Employment> findByHighlightedAndStatusOrderByIdDesc(int highlighted, String status);
+
+    List<Employment> findBySalaryBetween(double s1, double s2);
+
+    List<Employment> findByStatusIn(List<String> statuses);
 
 }
